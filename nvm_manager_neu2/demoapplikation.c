633f173497d2dd300601c3a517a43fff9ed2d412
Sami Taieb <sami.taieb@outlook.de>
2023-01-17 23:13:19 +0100

    demo angepasst

--- nvm_manager_neu2/demoapplikation.c
+++ nvm_manager_neu2/demoapplikation.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -15,64 +15,80 @@
 int NVM_demoapplikation() {
     NVMManager manager;
     int id;
-    int ret;    //Return der Funktionen gek�rzt auf "ret" weil "return" reserviert ist
+    int ret;    //Return der Funktionen gekürzt auf "ret" weil "return" reserviert ist
     unsigned char data[] = { 1, 2, 3, 4, 5 };
     NVMRecord record = { .header.length = sizeof(data) };
 
-    printf("\n\n\033[0;36m Start der Demoapplikation \n\033[0;37m");
+    printf("\n\n\033[0;36mStart der Demoapplikation \n\033[0;37m\n");
     printf("\033[0;37m");
 
     // Initialisiere den NVM-Manager
     ret = NVM_Init(&manager);
     assert(ret == 0);
+    printf("\033[0;32mInitialisiere den NVM-Manager: CHECK!\033[0;37m\n");
 
-    // Teste das Hinzuf�gen eines neuen Records
+    // Teste das Hinzufügen eines neuen Records
     id = NVM_AddNewRecord(&manager, &record, false, false);
     assert(id != -1);
+    printf("\033[0;32mHinzufügen eines neuen Records: CHECK!\033[0;37m\n");
+
+    // Teste das lesen aus dem unbeschribenen Record durch SyncReadRecord
+    ret = NVM_SyncReadRecord(&manager, &record);
+    assert(ret == -1);
+    printf("\033[0;32mlesen aus dem unbeschribenen Record durch SyncReadRecord: CHECK!\033[0;37m\n");
 
     // Teste das schreiben in den Record durch SyncWriteRecord
     memcpy(record.data, data, sizeof(data));
     ret = NVM_SyncWriteRecord(&manager, &record);
     assert(ret == 0);
+    printf("\033[0;32mschreiben in den Record durch SyncWriteRecord: CHECK!\033[0;37m\n");
 
     // Teste das lesen aus dem Record durch SyncReadRecord
     ret = NVM_SyncReadRecord(&manager, &record);
     assert(ret == 0);
+    printf("\033[0;32mlesen aus dem Record durch SyncReadRecord: CHECK!\033[0;37m\n");
 
-    // Teste das L�schen eines Records
+    // Teste das Löschen eines Records
     ret = NVM_DeleteRecord(&manager, &record);
     assert(ret == 0);
+    printf("\033[0;32mLöschen eines Records: CHECK!\033[0;37m\n");
 
-    // Teste das Hinzuf�gen eines schreibgesch�tzten Records
+    // Teste das Hinzufügen eines schreibgeschützten Records
     id = NVM_AddNewRecord(&manager, &record, true, false);
     memcpy(record.data, data, sizeof(data));
     NVM_SyncWriteRecord(&manager, &record);
     assert(id != -1);
+    printf("\033[0;32mHinzufügen eines schreibgeschützten Records: CHECK!\033[0;37m\n");
 
-    // Teste das �berschreiben eines schreibgesch�tzen Records
+    // Teste das Überschreiben eines schreibgeschützen Records
     memcpy(record.data, data, sizeof(data));
     ret = NVM_SyncWriteRecord(&manager, &record);
     assert(ret == -1);
+    printf("\033[0;32mÜberschreiben eines schreibgeschützen Records: CHECK!\033[0;37m\n");
 
-    // Teste das L�schen eines schreibgesch�tzten Records
+    // Teste das Löschen eines schreibgeschützten Records
     ret = NVM_DeleteRecord(&manager, &record);
     assert(ret == -1);
+    printf("\033[0;32mLöschen eines schreibgeschützten Records: CHECK!\033[0;37m\n");
 
-    // Teste das Hinzuf�gen eines redundanten Records
+    // Teste das Hinzufügen eines redundanten Records
     id = NVM_AddNewRecord(&manager, &record, false, true);
     memcpy(record.data, data, sizeof(data));
     NVM_SyncWriteRecord(&manager, &record);
     assert(id != -1);
+    printf("\033[0;32mHinzufügen eines redundanten Records: CHECK!\033[0;37m\n");
 
-    // Teste das L�schen eines redundanten Records
+    // Teste das Löschen eines redundanten Records
     ret = NVM_DeleteRecord(&manager, &record);
     assert(ret == 0);
+    printf("\033[0;32mLöschen eines redundanten Records: CHECK!\033[0;37m\n");
 
     // Teste das Reorganisieren der Records
     ret = NVM_ReorganizeRecords(&manager);
     assert(ret == 0);
+    printf("\033[0;32mReorganisieren der Records: CHECK!\033[0;37m\n");
 
-    // Teste das Hinzuf�gen von Records, bis kein Platz mehr im NVM-Speicher ist
+    // Teste das Hinzufügen von Records, bis kein Platz mehr im NVM-Speicher ist
     int i;
     for (i = 1; i < ALLOC_TABLE_SIZE; i++) {
         id = NVM_AddNewRecord(&manager, &record, false, false);
@@ -81,6 +97,17 @@ int NVM_demoapplikation() {
         }
     }
     assert(i == ALLOC_TABLE_SIZE);
+    printf("\033[0;32mHinzufügen von Records, bis kein Platz mehr im NVM-Speicher ist: CHECK!\033[0;37m\n");
+
+    // Teste
+    //ret = NVM_AsyncWriteRecord(&manager, id, &data, &record, 0);
+    //assert(ret == 0);
+
+    // Teste 
+    //ret = NVM_AsyncReadRecord(&manager, id, &data, &record, 0);
+    //assert(ret == 0);
+
+    //Teste Warteschlange voll
 
     return 0;
 }
